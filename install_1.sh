da13b23d20b2c18a34e6ea12c204fe48037c049e
Markus Ziller <mail.markus.ziller@gmail.com>
2017-05-23 18:45:24 +0200

    .gitignore

--- install_1.sh
+++ install_1.sh
@@ -10,7 +10,7 @@ wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-re
 dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
 apt-get update
 apt-get install cuda-toolkit-8-0
-usermod -a -G video $USER
+usermod -a -G video $(whoami)
 echo "" >> ~/.bashrc
 echo "export PATH=/usr/local/cuda-8.0/bin:$PATH" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda8.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
